c7e5abca9dd0d4726c52a44c3776f0f6cdb600ff
Nikhil Kale <nikkale1@in.ibm.com>
2021-04-11 14:38:38 +0530

    Added JSON marshal example

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/GoesToEleven/golang-arch
 
 go 1.15
+
+require golang.org/x/tools/gopls v0.6.9 // indirect
---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+type person struct {
+	First string
+}
+
+func main() {
+
+	p1 := person{
+		First: "Nikhil",
+	}
+
+	p2 := person{
+		First: "Ishaan",
+	}
+
+	xp := []person{p1, p2}
+
+	bs, err := json.Marshal(xp)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	fmt.Println(string(bs))
+
+}
