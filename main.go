7e319a20fe3eb9ca7aab489266453dedd6427941
Nikhil Kale <nikkale1@in.ibm.com>
2021-04-11 14:47:30 +0530

    Updated with Unmarshal

--- main.go
+++ main.go
@@ -27,6 +27,15 @@ func main() {
 		log.Panic(err)
 	}
 
-	fmt.Println(string(bs))
+	fmt.Println("Print JSON", string(bs))
+
+	xp2 := []person{}
+
+	err = json.Unmarshal(bs, &xp2)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	fmt.Println("Data in Go data structure ", xp2)
 
 }
